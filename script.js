30a0f8292cd2ba5fdf90109a80dd9101c3be3513
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2024-10-21 23:22:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Olá Mundo!")
+console.log(3*5)
+
+//var aluno = vinicius <-- Não utilizamos mais a variavel do tipo var
+let aluno = "vinicius";
+
+//variavel const não podem ser reatribuidas
+const cpf = 88888888;
+
+//boolean = true ou false
+let nome = false;
+console.log(nome);
+
+//null = nulo ou inexistente
+let vini = null;
+console.log(vini)
+
+//undefined = ainda não definido
+let dia;
+console.log(dia);
+
+//dia = 20
+//console.log(dia)
+
+//NaN = não é um número
+console.log(5/"y")
+
+//tipos de dados
+
+//string = textos
+//number = números
+// boolean - true ou false
+//null = nulo ou inexistente
+//NaN = não é um número
+//undefined = ainda não definido
